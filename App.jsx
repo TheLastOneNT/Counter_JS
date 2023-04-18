f184f41f3a414ba35ce71d39449f867b795e05c4
The Last One <thelastonent@gmail.com>
2023-04-17 22:18:26 -0400

    react-hook

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="counter">
+      <div className="count">
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
